235100dbcbb3f4e40d67ea105472dfa7b4e6b78e
CelsoGasparin <134729228+CelsoGasparin@users.noreply.github.com>
2024-07-07 18:55:15 -0300

    Add files via upload

--- O.O2TDS/Aulas/aula_4.php
+++ O.O2TDS/Aulas/aula_4.php
@@ -1,10 +1,8 @@
 <?php
 
 
+print ceaser("Brasil",7);
 
-
-
-print ceasar("brasil",7);
 
                         
 
@@ -14,29 +12,20 @@ print ceasar("brasil",7);
 
 
 
-function ceasar($text,int $key){
+function ceaser($text,int $key){
     $A = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     $a = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     $txtlen = strlen($text); 
     $txt = "";
     $key %= 26;
     for($i=0; $i < $txtlen; $i++){ 
-        $c = substr($text,$i,1);
-        //print $c;
-        
+        $c = substr($text,$i,$i+1);
         for($j=0; $j < 26; $j++) { 
-            if(($c == $A[$j] or $c == $a[$j]) and ($j+$key <= 25 and $j+$key >= 0)){
-                $txt .= $a[$j+$key];
+            if($c == $A[$j] or $c == $a[$j]){
+                $txt += $a[$j];
                 break;
-                
-            }elseif(($c == $A[$j] or $c == $a[$j]) and $j+$key > 25){
-                $txt .= $a[0+($key-(26-$j))];
-                break;
-            }elseif(($c == $A[$j] or $c == $a[$j]) and $j+$key < 0){
-                $txt .= $a[26+($key+$j)];
-                break;                
             }elseif($j == 25){
-                $txt .= $c;
+                $txt += $c;
             }
         }
 
